4785de20c465e5954ca157e36e3d5ff4427e9fea
Jeff <lijefferson027@gmail.com>
2025-02-26 23:14:46 -0500

    updated the deployment script

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -9,7 +9,7 @@ cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
 
 echo "Copying files to server..."
 sshpass -f ../pw.txt scp -r *.js "$SERVER_USER@$SERVER_IP:$REMOTE_DIR" || { echo "File transfer failed!"; exit 1; }
-sshpass -f ../pw.txt scp -r models/*.js "$SERVER_USER@$SERVER_IP:$REMOTE_DIR" || { echo "File transfer failed!"; exit 1; }
-sshpass -f ../pw.txt scp -r routes/*.js "$SERVER_USER@$SERVER_IP:$REMOTE_DIR" || { echo "File transfer failed!"; exit 1; }
+sshpass -f ../pw.txt scp -r models/*.js "$SERVER_USER@$SERVER_IP:$REMOTE_DIR/models/" || { echo "File transfer failed!"; exit 1; }
+sshpass -f ../pw.txt scp -r routes/*.js "$SERVER_USER@$SERVER_IP:$REMOTE_DIR/routes/" || { echo "File transfer failed!"; exit 1; }
 
 echo "Deployment successful!"
