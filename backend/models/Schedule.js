0f763453e8d8167452c916c2a5f6fc9ee91713de
Jeff <lijefferson027@gmail.com>
2025-03-04 18:58:04 -0500

    getclass endpoint works

--- backend/models/Schedule.js
+++ backend/models/Schedule.js
@@ -1,7 +1,9 @@
 //schedule schema
 const mongoose = require('mongoose');
+ObjectId = mongoose.Schema.Types.ObjectId;
 
 const ScheduleSchema = new mongoose.Schema({
+  _id: { type: ObjectId, required: false },
   course_code: { type: String, required: true },
   class_name: { type: String, required: true},
   professor: { type: String, required: true },
--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 //Add a new class endpoint
 router.post("/addClass", async (req, res) => {
-  const { course_code, class_name, professor, meeting_type, type, Building, class_schedule, Building_Prefix, Room_Number } = req.body;
+  const { course_code, class_name, professor, meeting_type, type, building, class_schedule, building_prefix, room_number } = req.body;
 
   try {
     const newClass = new Schedule({
@@ -32,3 +32,20 @@ router.post("/addClass", async (req, res) => {
 //Delete a class endpoint
 
 //Get class by ID endpoint
+router.get("/getClass/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  try {
+    const classData = await Schedule.findById(_id);
+    if (!classData) {
+      return res.status(404).json({ message: "Class not found" });
+    }
+    res.json(classData);
+  } catch (error) {
+    res.status(500).json({ message: `Server error: ${error}` });
+  }
+}
+);
+
+
+module.exports = router;
