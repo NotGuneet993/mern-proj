8a2e376249b3a28b6bcbfae3ae446e3a8569d190
Jeff <lijefferson027@gmail.com>
2025-03-05 20:35:08 -0500

    add modal and related changes

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,42 @@
-export default function DashboardPage() {
+import React, { useState } from 'react';
+import AddModal from '../temp_Modal/AddModal';
 
-    return (
-        <p>Dashboard</p>
-    );
+function DashboardPage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // Suppose you keep a local list of classes:
+  const [classes, setClasses] = useState<ClassData[]>([]);
+
+  interface ClassData {
+    course_code: string;
+    class_name: string;
+    professor: string;
+  }
+
+  const handleAddClass = (newClassData: ClassData) => {
+    //it is curretly not saving the data to the database
+    //we can call the schedule addClass api to add the class
+    setClasses((prev) => [...prev, newClassData]);
+  };
+  return (
+    <div>
+      <button className='border-4 bg-gray-200' onClick={() => setModalOpen(true)}>Add Class</button>
+
+      {/* Display current classes */}
+      {classes.map((cls, index) => (
+        <div key={index}>
+          <h4>{cls.course_code} - {cls.class_name}</h4>
+          <p>Professor: {cls.professor}</p>
+        </div>
+      ))}
+
+      <AddModal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onSave={handleAddClass}
+      />
+    </div>
+  );
 }
+
+export default DashboardPage;
