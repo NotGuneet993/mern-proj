9d9fa878a246dd15fd47f62446d3a044cb4a5604
Guneet <guberoi@icloud.com>
2025-03-28 14:58:22 -0400

    added menu and swapped modal buttons

--- frontend/src/components/PlusButtonMenu.tsx
+++ frontend/src/components/PlusButtonMenu.tsx
@@ -1,19 +1,36 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 
-export default function PlusButtonMenu() {
+const API_URL = import.meta.env.VITE_API_URL;
+
+interface PlusButtonMenuProps {
+    setModalOpen: (auth: boolean) => void;
+}
+
+export default function PlusButtonMenu({ setModalOpen } : PlusButtonMenuProps) {
 
     const [rotated, setRotated] = useState(false);
 
     return (
         <div
-            onClick={() => setRotated(!rotated)}
-            className="border rounded-full bg-stone-100 text-4xl p-3 hover:border-amber-500 cursor-pointer "
+            className={`w-[${rotated? 150 : 50}px] h-[${rotated? 200 : 50}px] bg-stone-100 border-1 border-amber-500 
+                        rounded-3xl transition-all duration-350 ease-in-out pt-[15px]`}
         >
+            {rotated && (
+                <div className="flex flex-col items-center">
+                    <p className="text-lg my-1 hover:text-blue-500 hover:cursor-pointer"
+                    onClick={() => setModalOpen(true)}>
+                        Add Class
+                    </p>
+
+                    <p className="text-lg my-1 hover:text-blue-500 hover:cursor-pointer">Schedule</p>
+                </div>
+            )}
             <FaPlus
-                className={`text-black transition-transform duration-300 ease-in-out hover:text-amber-500 ${
+                className={`absolute text-black text-[40px] left-[5px] bottom-[5px] transition-transform duration-350 ease-in-out hover:text-amber-500 ${
                     rotated ? 'rotate-45' : '-rotate-0'
                 }`}
+                onClick={() => setRotated(!rotated)}
             />
         </div>
     );
